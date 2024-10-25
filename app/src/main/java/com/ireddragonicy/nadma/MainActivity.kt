cffc02b26e2ae57eff53ee5f015d78f4bef50ced
IRedDragonICY <hendik.suwoto@gmail.com>
2024-10-26 04:32:17 +0800

    Initial commit

---
+++ app/src/main/java/com/ireddragonicy/nadma/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.ireddragonicy.nadma
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ireddragonicy.nadma.ui.theme.NadmaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NadmaTheme {
+
+            }
+        }
+    }
+}
+
