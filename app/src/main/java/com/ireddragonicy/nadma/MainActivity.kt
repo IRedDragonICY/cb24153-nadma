364980172749a697f8ee5e316a20d677cbd531ac
IRedDragonICY <hendik.suwoto@gmail.com>
2024-10-27 13:14:44 +0800

    refactor: update color scheme and adjust scale (main)
    - Changed nav item color from purple_500 to blue_500.
    - Updated color definitions in colors.xml.
    - Adjusted visualization tool scale in misc.xml.
    - Removed unused import in MainActivity.kt.

--- app/src/main/java/com/ireddragonicy/nadma/MainActivity.kt
+++ app/src/main/java/com/ireddragonicy/nadma/MainActivity.kt
@@ -3,7 +3,6 @@ package com.ireddragonicy.nadma
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
-import com.ireddragonicy.nadma.ui.theme.NadmaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
