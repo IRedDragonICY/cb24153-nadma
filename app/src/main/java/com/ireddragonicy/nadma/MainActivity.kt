5a017d0b3080a7adf0000b85c20b35e9f210e95a
IRedDragonICY <hendik.suwoto@gmail.com>
2024-10-27 13:05:24 +0800

    feat: update layouts and theme customization (main)
    - Changed visualization tool project scale in misc.xml.
    - Added new layout file activity_main.xml.
    - Updated themes.xml for better theme structure and customization.
    - Replaced TextViews with BottomNavigationView in navbar.xml.
    - Modified MainActivity to set content view to activity_main.

--- app/src/main/java/com/ireddragonicy/nadma/MainActivity.kt
+++ app/src/main/java/com/ireddragonicy/nadma/MainActivity.kt
@@ -2,18 +2,15 @@ package com.ireddragonicy.nadma
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.ireddragonicy.nadma.ui.theme.NadmaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            NadmaTheme {
 
-            }
-        }
+        enableEdgeToEdge()
+
+        setContentView(R.layout.activity_main)
     }
 }
