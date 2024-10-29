9706e855ef3198a6b0e1a87863729b36351e1b82
IRedDragonICY <hendik.suwoto@gmail.com>
2024-10-29 12:52:47 +0800

    refactor(home): update header layout and dynamic date handling
    - Renamed layout file: header.xml to header_home.xml
    - Added IDs to TextViews in header_home.xml for day, month, and year
    - Updated HomeFragment to use FragmentHomeBinding
    - Implemented dynamic date setting in HomeFragment

--- app/src/main/java/com/ireddragonicy/nadma/HomeFragment.kt
+++ app/src/main/java/com/ireddragonicy/nadma/HomeFragment.kt
@@ -1,17 +1,44 @@
 package com.ireddragonicy.nadma
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import com.ireddragonicy.nadma.databinding.FragmentMapsBinding
+import android.view.ViewGroup
+import java.util.Locale
+import androidx.fragment.app.Fragment
+import com.ireddragonicy.nadma.databinding.FragmentHomeBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
 
-class HomeFragment : BaseFragment(R.layout.fragment_home) {
+class HomeFragment : Fragment() {
 
-    private var _binding: FragmentMapsBinding? = null
+    private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
+    private val dateFormatDay = SimpleDateFormat("dd", Locale.getDefault())
+    private val dateFormatMonth = SimpleDateFormat("MMM", Locale.getDefault())
+    private val dateFormatYear = SimpleDateFormat("yyyy", Locale.getDefault())
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        _binding = FragmentMapsBinding.bind(view)
-        // TODO: Implement your logic here
+        super.onViewCreated(view, savedInstanceState)
+
+        setDynamicDate()
+    }
+
+    private fun setDynamicDate() {
+        val calendar = Calendar.getInstance()
+
+        binding.headerHome.textDay.text = dateFormatDay.format(calendar.time)
+        binding.headerHome.textMonth.text = dateFormatMonth.format(calendar.time).uppercase(Locale.getDefault())
+        binding.headerHome.textYear.text = dateFormatYear.format(calendar.time)
     }
 
     override fun onDestroyView() {
